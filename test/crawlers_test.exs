d2726c6e99437ccad162f57889feca4ee2ea5066
Robb Shecter <robb@public.law>
2022-09-20 22:43:36 -0600

    refactor: DRY tests

--- test/crawlers_test.exs
+++ test/crawlers_test.exs
@@ -6,19 +6,17 @@ defmodule CrawlersTest do
   doctest Crawlers
 
   setup_all do
-    {:ok, document} = Floki.parse_document(File.read!("test/fixtures/ors.aspx"))
+    {:ok, document} = "test/fixtures/ors.aspx" |> File.read!() |> Floki.parse_document()
+    volumes = Parser.volumes(document)
 
-    %{doc: document}
+    %{doc: document, volumes: volumes}
   end
 
-  test "finds the correct # of Volumes", %{doc: doc} do
-    volumes = Parser.volumes(doc)
-
+  test "finds the correct # of Volumes", %{volumes: volumes} do
     assert Enum.count(volumes) == 19
   end
 
-  test "gets Volume 1 name", %{doc: doc} do
-    volumes = Parser.volumes(doc)
+  test "gets Volume 1 name", %{volumes: volumes} do
     vol_1 = List.first(volumes)
 
     assert vol_1.name == "Courts, Oregon Rules of Civil Procedure"
