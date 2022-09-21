79a15d95b76eabec1c5f546abc61f8df5d502ae4
Robb Shecter <robb@public.law>
2022-09-20 22:04:34 -0600

    failing test for Volume count

--- test/crawlers_test.exs
+++ test/crawlers_test.exs
@@ -1,8 +1,21 @@
 defmodule CrawlersTest do
+  @moduledoc """
+  Test the ORS crawler.
+  """
   use ExUnit.Case
   doctest Crawlers
 
+  setup_all do
+    %{html: File.read!("test/fixtures/ors.aspx")}
+  end
+
   test "greets the world" do
     assert Crawlers.hello() == :world
+  end
+
+  test "gets the correct # of Volumes", %{html: html} do
+    dbg(html)
+
+    assert List.length(Parser.parse(html).volumes) == 8
   end
 end
