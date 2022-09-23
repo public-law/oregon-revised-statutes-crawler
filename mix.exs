c2dba525d7efaaf9f7662970eb8b4927e2029532
Robb Shecter <robb@public.law>
2022-09-22 20:03:27 -0600

    remove erlyconv

--- mix.exs
+++ mix.exs
@@ -27,8 +27,7 @@ defmodule Crawlers.MixProject do
       {:floki, "~> 0.33.0"},
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.4"},
-      {:typed_struct, "~> 0.3.0", runtime: false},
-      {:erlyconv, github: "eugenehr/erlyconv"}
+      {:typed_struct, "~> 0.3.0", runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
