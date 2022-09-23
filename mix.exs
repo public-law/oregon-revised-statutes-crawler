095e874ded9d54997f6938397fae567dfde236c4
Robb Shecter <robb@public.law>
2022-09-23 00:35:37 -0600

    add test.watch

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Crawlers.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        "test.watch": :test
+      ]
     ]
   end
 
@@ -27,6 +30,7 @@ defmodule Crawlers.MixProject do
       {:floki, "~> 0.33.0"},
       {:httpoison, "~> 1.7"},
       {:jason, "~> 1.4"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:typed_struct, "~> 0.3.0", runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
