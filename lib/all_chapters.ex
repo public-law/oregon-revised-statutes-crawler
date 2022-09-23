215666d52b5682e649c3410305f71afd0171573e
Robb Shecter <robb@public.law>
2022-09-22 19:22:47 -0600

    docs

--- lib/all_chapters.ex
+++ lib/all_chapters.ex
@@ -1,14 +1,11 @@
 defmodule AllChapters do
   @moduledoc """
-  First, make sure you add HTTPoison to your mix.exs dependencies:
-
-  def deps do
-    [{:httpoison, "~> 0.10.0"}]
-  end
+  ORS API client.
   """
 
   @doc """
   All Chapters
+
   `POST https://www.oregonlegislature.gov/bills_laws/_layouts/15/inplview.aspx?List=%7B77CEA715-9752-4B71-90C7-0417BB45D7BB%7D&View=%7BD854F19F-B98E-4414-8559-316FAEC4CA86%7D&IsXslView=TRUE&IsCSR=TRUE&IsGroupRender=TRUE`
   """
   def request do
