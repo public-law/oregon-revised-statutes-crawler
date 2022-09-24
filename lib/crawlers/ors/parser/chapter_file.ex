3049e6b9652833b1fd076c3710faed04e693465a
Robb Shecter <robb@public.law>
2022-09-23 21:02:21 -0600

    test pass

--- lib/crawlers/ors/parser/chapter_file.ex
+++ lib/crawlers/ors/parser/chapter_file.ex
@@ -1,4 +1,5 @@
 import Enum, only: [map: 2]
+import String, only: [split: 3, trim: 1, trim_trailing: 2]
 
 defmodule Parser.ChapterFile do
   @moduledoc """
@@ -21,6 +22,9 @@ defmodule Parser.ChapterFile do
   defp extract_heading_info(headings) do
     headings
     |> map(&Floki.text/1)
-    |> dbg
+    |> map(&trim/1)
+    |> map(&trim_trailing(&1, "."))
+    |> map(&split(&1, "\r\n", parts: 2))
+    |> map(fn [number, name] -> %{name: name, number: number} end)
   end
 end
