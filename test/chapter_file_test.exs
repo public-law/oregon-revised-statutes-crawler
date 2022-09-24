4f2331162be40d687d01d3b0eabe82d2efa93d9f
Robb Shecter <robb@public.law>
2022-09-23 20:11:57 -0600

    ors chap file fixture

---
+++ test/chapter_file_test.exs
@@ -0,0 +1,69 @@
+defmodule ChapterFileTest do
+  @moduledoc """
+  Test ability to parse a chapter file's sections.
+  """
+  import List
+  use ExUnit.Case, async: true
+
+  setup_all do
+    api_result =
+      "test/fixtures/ors-chapters.json"
+      |> File.read!()
+      |> Jason.decode!()
+      |> Map.get("Row")
+
+    %{chapters: Parser.chapters(api_result)}
+  end
+
+  test "finds the correct # of Sections", %{sections: sections} do
+    assert Enum.count(sections) == 688
+  end
+
+  # test "Chapter 1 name", %{chapters: chapters} do
+  #   first_chapter = first(chapters)
+
+  #   assert first_chapter.name == "Courts and Judicial Officers Generally"
+  # end
+
+  # test "Chapter 1 number", %{chapters: chapters} do
+  #   first_chapter = first(chapters)
+
+  #   assert first_chapter.number == "1"
+  # end
+
+  # test "Chapter 1 Title number", %{chapters: chapters} do
+  #   first_chapter = first(chapters)
+
+  #   assert first_chapter.title_number == "1"
+  # end
+
+  # test "Chapter 1 URL", %{chapters: chapters} do
+  #   first_chapter = first(chapters)
+
+  #   assert first_chapter.url == "https://www.oregonlegislature.gov/bills_laws/ors/ors001.html"
+  # end
+
+  # test "Last Chapter name", %{chapters: chapters} do
+  #   last_chapter = last(chapters)
+
+  #   assert last_chapter.name == "Airport Districts"
+  # end
+
+  # test "Last Chapter number", %{chapters: chapters} do
+  #   last_chapter = last(chapters)
+
+  #   assert last_chapter.number == "838"
+  # end
+
+  # test "Last Chapter Title number", %{chapters: chapters} do
+  #   last_chapter = last(chapters)
+
+  #   assert last_chapter.title_number == "62"
+  # end
+
+  # test "Last Chapter URL", %{chapters: chapters} do
+  #   last_chapter = last(chapters)
+
+  #   assert last_chapter.url == "https://www.oregonlegislature.gov/bills_laws/ors/ors838.html"
+  # end
+end
