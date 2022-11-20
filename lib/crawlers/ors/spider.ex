5d9654a042c49d95e2141275772ab9ed8d261ccb
Robb Shecter <robb@public.law>
2022-11-20 05:32:36 -0700

    spider changes

--- lib/crawlers/ors/spider.ex
+++ lib/crawlers/ors/spider.ex
@@ -5,19 +5,20 @@ defmodule Spider do
   use Crawly.Spider
   alias Parser.ChapterFile
 
-  @home_page "https://www.oregonlegislature.gov/bills_laws/Pages/ORS.aspx"
+  @ors_home_page "https://www.oregonlegislature.gov/bills_laws/Pages/ORS.aspx"
   @chapter_root "https://www.oregonlegislature.gov/bills_laws/ors/"
+  @annos_home_page "https://www.oregonlegislature.gov/bills_laws/Pages/Annotations.aspx"
 
   @impl Crawly.Spider
   def base_url, do: "https://www.oregonlegislature.gov/"
 
   @impl Crawly.Spider
   def init do
-    [start_urls: [@home_page]]
+    [start_urls: [@ors_home_page, @annos_home_page]]
   end
 
   @impl Crawly.Spider
-  def parse_item(%{request_url: @home_page} = response) do
+  def parse_item(%{request_url: @ors_home_page} = response) do
     Parser.parse_home_page(response)
   end
 
@@ -26,4 +27,9 @@ defmodule Spider do
 
     ChapterFile.parse(response)
   end
+
+  def parse_item(%{request_url: @annos_home_page} = response) do
+    AnnoHomePage.parse(response)
+  end
+
 end
