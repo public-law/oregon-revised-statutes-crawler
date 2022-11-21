f49c48b0bfdba3f604b81f98cd32e2ada132c6fd
Robb Shecter <robb@public.law>
2022-11-20 22:16:48 -0700

    first failing test

--- lib/crawlers/ors/parser/anno_home_page.ex
+++ lib/crawlers/ors/parser/anno_home_page.ex
@@ -1,6 +1,12 @@
 defmodule Parser.AnnoHomePage do
+  @moduledoc """
+  Parse https://www.oregonlegislature.gov/bills_laws/Pages/Annotations.aspx
+  """
 
   def parse(_) do
   end
 
+  def annotations(_) do
+    []
+  end
 end
