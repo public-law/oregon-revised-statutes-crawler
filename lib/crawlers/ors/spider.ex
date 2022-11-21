0c7bdfe37e18bac42b0c06a85bd06bf445fdf58e
Robb Shecter <robb@public.law>
2022-11-21 00:15:35 -0700

    refactor style

--- lib/crawlers/ors/spider.ex
+++ lib/crawlers/ors/spider.ex
@@ -12,24 +12,29 @@ defmodule Spider do
   @chapter_root    "https://www.oregonlegislature.gov/bills_laws/ors/ors"
   @anno_root       "https://www.oregonlegislature.gov/bills_laws/ors/ano"
 
+
   @impl Crawly.Spider
   def base_url, do: "https://www.oregonlegislature.gov/"
+
 
   @impl Crawly.Spider
   def init do
     [start_urls: [@ors_home_page]]
   end
 
+
   @impl Crawly.Spider
   def parse_item(%{request_url: @ors_home_page} = response) do
     Parser.parse_home_page(response)
   end
+
 
   def parse_item(%{request_url: @chapter_root <> _} = response) do
     Logger.info("Parsing #{response.request_url}...")
 
     ChapterFile.parse(response)
   end
+
 
   def parse_item(%{request_url: @anno_root <> _} = response) do
     Logger.info("Parsing #{response.request_url}...")
