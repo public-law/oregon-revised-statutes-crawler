e9a166d57d7b1e7d1896bf9c7b6418bbe419b36b
Robb Shecter <robb@public.law>
2023-03-07 23:45:43 -0700

    group by section headings

--- lib/crawlers/ors/parser/annotation_file.ex
+++ lib/crawlers/ors/parser/annotation_file.ex
@@ -2,20 +2,33 @@ defmodule Parser.AnnotationFile do
   @moduledoc """
   They're listed at https://www.oregonlegislature.gov/bills_laws/Pages/Annotations.aspx
   """
+  import String, only: [replace: 3]
   alias Crawlers.ORS.Models.ChapterAnnotation
+
 
   def parse(_) do
   end
 
+
   @spec chapter_annotations(Floki.html_tree()) :: [ChapterAnnotation.t()]
   def chapter_annotations(dom) do
-    paragraphs =
+    annotations =
       dom
       |> Floki.find("p")
       |> Enum.map(&Floki.text/1)
+      |> Enum.map(fn s -> replace(s, <<194, 160>>, " ") end)
+      |> Util.group_with(&section_heading?/1)
 
-    paragraphs |> dbg
+    annotations |> dbg
 
     []
+  end
+
+
+  #
+  # E.g.: "      2.570"
+  #
+  defp section_heading?(paragraph) when is_binary(paragraph) do
+    paragraph =~ ~r/^.     \w+\.\w+$/
   end
 end
