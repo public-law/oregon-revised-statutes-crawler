835a8f2cf6f121667e92c0d44047e1ce25d6a9f2
Robb Shecter <robb@public.law>
2023-07-28 23:43:12 -0600

    return the list of URLs

--- lib/law_pdfs.ex
+++ lib/law_pdfs.ex
@@ -1,8 +1,9 @@
-defmodule SendRequest do
+defmodule LawPdfs do
   @moduledoc """
   Session laws API client.
   """
 
+  @spec request :: list | HTTPoison.Error.t()
   @doc """
   # Request
   # POST https://www.oregonlegislature.gov/bills_laws/_layouts/15/inplview.aspx?List=%7B88BF04C7-3C52-4FFA-9717-94016EC3B24E%7D&View=%7B3BEB1821-2C16-4437-85F5-00046A0A96E7%7D&IsXslView=TRUE&IsCSR=TRUE&GroupString=%3B%232022%20Regular%3B%23&IsGroupRender=TRUE
@@ -28,11 +29,11 @@ defmodule SendRequest do
 
     HTTPoison.start()
     case HTTPoison.post(url, body, headers, params: params) do
-      {:ok, response = %HTTPoison.Response{status_code: status_code, body: body}} ->
-        IO.puts("Response Status Code: #{status_code}")
-        IO.puts("Response Body: #{body}")
+      {:ok, _response = %HTTPoison.Response{status_code: _status_code, body: body}} ->
+        Jason.decode!(body)
+        |> Map.get("Row")
+        |> Enum.map(fn item -> item["TitleURL"] end)
 
-        response
       {:error, error = %HTTPoison.Error{reason: reason}} ->
         IO.puts("Request failed: #{reason}")
 
