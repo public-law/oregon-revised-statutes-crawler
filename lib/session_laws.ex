0d30f86a8ad51181abb52e8c8fc24eb49f63a990
Robb Shecter <robb@public.law>
2023-07-29 00:17:54 -0600

    refactor

--- lib/session_laws.ex
+++ lib/session_laws.ex
@@ -3,15 +3,28 @@ defmodule SessionLaws do
   Session laws API client.
   """
 
-  @spec pdf_paths(integer, binary) :: list | HTTPoison.Error.t()
-  @doc """
-  E.g.,
-    `SessionLaws.pdf_paths(2021, "Special 2")`
-    `SessionLaws.pdf_paths(2022, "Regular")`
-  """
-  def pdf_paths(year, session) when is_integer(year) and is_binary(session) do
-    url = "https://www.oregonlegislature.gov/bills_laws/_layouts/15/inplview.aspx"
+  @url "https://www.oregonlegislature.gov/bills_laws/_layouts/15/inplview.aspx"
 
+
+  @spec special_session_pdfs(integer, integer) :: list | HTTPoison.Error.t()
+  def special_session_pdfs(year, session_number) when is_integer(session_number) do
+    pdf_paths(year, "Special #{session_number}")
+  end
+
+
+  @spec regular_session_pdfs(integer) :: list | HTTPoison.Error.t()
+  def regular_session_pdfs(year) do
+    pdf_paths(year, "Regular")
+  end
+
+
+  @spec pdf_paths(integer, binary) :: list | HTTPoison.Error.t()
+  # @doc """
+  # E.g.,
+  #   `SessionLaws.pdf_paths(2021, "Special 2")`
+  #   `SessionLaws.pdf_paths(2022, "Regular")`
+  # """
+  defp pdf_paths(year, session) when is_integer(year) and is_binary(session) do
     # ====== Headers ======
     headers = []
 
@@ -29,7 +42,7 @@ defmodule SessionLaws do
     body = ""
 
     HTTPoison.start()
-    case HTTPoison.post(url, body, headers, params: params) do
+    case HTTPoison.post(@url, body, headers, params: params) do
       {:ok, _response = %HTTPoison.Response{status_code: _status_code, body: body}} ->
         Jason.decode!(body)
         |> Map.get("Row")
