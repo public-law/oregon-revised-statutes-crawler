449d0459c2aa28030e36972c4462269250f661f8
Robb Shecter <robb@public.law>
2023-08-02 16:55:59 -0600

    returning correct urls

--- lib/crawlers/oar/parser.ex
+++ lib/crawlers/oar/parser.ex
@@ -2,17 +2,27 @@ defmodule Crawlers.Oar.Parser do
 
   @spec parse_from_api :: Crawly.ParsedItem.t()
   def parse_from_api() do
-
     # The sessions to add, August 2023.
-    results =
+    # TODO: Figure out an intelligent way to determine this.
+    raw_paths =
       SessionLaws.regular_session_pdfs(2022)
         ++ SessionLaws.special_session_pdfs(2021, 2)
         ++ SessionLaws.special_session_pdfs(2021, 1)
 
+    urls = raw_paths
+      |> Enum.map(&cleanup_path/1)
+
 
     %Elixir.Crawly.ParsedItem{
-      items:    results,
+      items:    urls,
       requests: []
     }
+  end
+
+
+  defp cleanup_path(path) do
+    path
+    |> String.replace("http://", "https://")
+    |> String.replace(~r/^\/bills_laws/, "https://www.oregonlegislature.gov/bills_laws")
   end
 end
