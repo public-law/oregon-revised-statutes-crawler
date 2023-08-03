fa8b4192cf852bf503faa11e754cfff7f9d2142d
Robb Shecter <robb@public.law>
2023-08-02 23:48:38 -0600

    add source url (#27)
    * added the source url
    
    * refactor
    
    * saving to the file
    
    * new filename

--- script/analyze_pdfs.exs
+++ script/analyze_pdfs.exs
@@ -10,24 +10,26 @@ defmodule AnalyzePdfs do
     |> String.split("\n")
     |> Enum.map(&String.trim/1)
     |> Enum.reject(&(String.length(&1) == 0))
-    |> Enum.map(&parse_to_json/1)
+    |> Enum.map(&parse_to_structure/1)
     |> Enum.map(&Jason.encode!/1)
-    |> Enum.map(&IO.puts/1)
+    |> File.write("tmp/ors-session-laws-metadata.json")
   end
 
 
-  def parse_to_json(url) do
-    IO.puts(:standard_error, "Parsing #{url}...")
-
-    json_text = case System.cmd("analyze", [url], into: "") do
+  def parse_to_structure(url) when is_binary(url) do
+    case run_analyzer(url) do
       {json_text, 0} ->
-        json_text
+        %{ source_url: url, metadata: Jason.decode!(json_text) }
 
       _ ->
-        "{ \"error\": \"#{url}\" }"
+        %{ error: url }
     end
+  end
 
-    Jason.decode!(json_text)
+
+  defp run_analyzer(url) do
+    IO.puts(:standard_error, "Parsing #{url}...")
+    System.cmd("analyze", [url], into: "")
   end
 end
 
