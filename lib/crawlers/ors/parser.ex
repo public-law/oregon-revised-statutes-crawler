636384f8eed6b84bc86f917ef7a0c2bc68655540
Robb Shecter <robb@public.law>
2023-08-11 18:45:26 -0600

    all tests fail

--- lib/crawlers/ors/parser.ex
+++ lib/crawlers/ors/parser.ex
@@ -73,7 +73,6 @@ defmodule Parser do
   end
 
 
-
   @spec chapters(any) :: [Chapter]
   def chapters(api_data) do
       api_data
@@ -81,17 +80,13 @@ defmodule Parser do
         url = "https://www.oregonlegislature.gov" <> Map.fetch!(c, "TitleURL")
         name = Map.fetch!(c, "ORS_x0020_Chapter_x0020_Title")
 
-        if name == "(Former Provisions)" do
-          nil
-        else
-          Chapter.new(
-            name: name,
-            number: Map.fetch!(c, "Title") |> capture(~r/Chapter (\w+)/) |> trim_leading("0"),
-            title_number: Map.fetch!(c, "ORS_x0020_Chapter") |> capture(~r/^([^.]+)/),
-            url: url,
-            anno_url: replace(url, "ors/ors", "ors/ano")
-          )
-        end
+        Chapter.new(
+          name: name,
+          number: Map.fetch!(c, "Title") |> capture(~r/Chapter (\w+)/) |> trim_leading("0"),
+          title_number: Map.fetch!(c, "ORS_x0020_Chapter") |> capture(~r/^([^.]+)/),
+          url: url,
+          anno_url: replace(url, "ors/ors", "ors/ano")
+        )
       end)
       |> Enum.reject(&is_nil/1)
       |> Util.cat_oks(&Logger.info/1)
