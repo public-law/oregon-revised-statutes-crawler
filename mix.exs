e6030374f51f21c80a3b981cdc91dacf28e62d9a
Robb Shecter <robb@public.law>
2023-12-11 23:10:56 -0700

    update

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Crawlers.MixProject do
   defp deps do
     [
       {:crawly, git: "https://github.com/elixir-crawly/crawly", branch: :master},
-      {:floki, "~> 0.33.0"},
+      {:floki, "~> 0.33"},
 
       {:credo, "> 0.0.0"},
       {:dialyxir, "> 1.0.0", runtime: false},
