6516ad118e5d6e7dd29fc53fecf65f890670bd9b
Robb Shecter <robb@public.law>
2023-12-26 19:59:58 -0700

    refactor (#36)
    * checkpoint
    
    * elixir 1.16
    
    * cleanup
    
    * use my fork of erlyconv
    
    * fix: CI

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Crawlers.MixProject do
       app: :crawlers,
       compilers: [:domo_compiler] ++ Mix.compilers(),
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: [
@@ -29,18 +29,21 @@ defmodule Crawlers.MixProject do
       {:floki, "~> 0.33.0"},
       {:logger_file_backend, github: "dbii/logger_file_backend", branch: "warn-fix"},
 
-      {:credo,    "> 0.0.0"},
-      {:dialyxir, "> 1.0.0", runtime: false},
+      # Dev tools
+      {:credo,          "> 0.0.0"},
+      {:dialyxir,       "> 1.0.0", runtime: false},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
 
-      {:erlyconv, github: "eugenehr/erlyconv"},
-      {:httpoison,      "~> 1.7"},
-      {:jason,          "~> 1.4"},
-      {:curl_ex,        "~> 1.2.0"},
+      {:erlyconv,  github: "dogweather/erlyconv", branch: "manually-fix-cp1006"},
+      {:httpoison, "~> 1.7"},
+      {:jason,     "~> 1.4"},
+      {:curl_ex,   "~> 1.2.0"},
 
+      # Typing and validation
       {:domo,         "~> 1.5"},
       {:typed_struct, "~> 0.3.0", runtime: false},
 
+      # The Crawly framework
       {:crawly, github: "elixir-crawly/crawly", branch: "master"},
     ]
   end
